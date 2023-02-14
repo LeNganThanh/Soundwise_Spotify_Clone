8058e118f9426efd2bc361e345ca28d1fc68d4db
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2023-02-14 19:17:00 +0100

    fixing the login image check

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react';
-import { useEffect } from 'react';
-import MainContext from '../../context/MainContext';
+import React, { useContext } from "react";
+import { useEffect } from "react";
+import MainContext from "../../context/MainContext";
 
-import { loginUrl } from '../../spotify';
-import classes from './Login.module.css';
+import { loginUrl } from "../../spotify";
+import classes from "./Login.module.css";
+import logo from "../../media/headphones-gradient.png";
 
 function Login() {
   const [STATE, DISPATCH] = useContext(MainContext);
   const { user, hashToken } = STATE;
-  console.log('This is the login', user);
   const getUser = async () => {
-    await fetch('https://api.spotify.com/v1/me', {
+    await fetch("https://api.spotify.com/v1/me", {
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + hashToken,
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + hashToken,
       },
     })
       .then(res => res.json())
       .then(res =>
         DISPATCH({
-          type: 'SET_USER',
+          type: "SET_USER",
           user: res,
         })
       );
@@ -37,12 +37,16 @@ function Login() {
           <a href={loginUrl}>LOGIN TO SPOTIFY</a>
         </div>
       ) : (
-        <div className={classes['login-button']}>
-          <img
-            className={classes.userImg}
-            src={user?.images[0].url}
-            alt="user_image"
-          />
+        <div className={classes["login-button"]}>
+          {user.images.length === 0 ? (
+            <img className={classes.userImg} src={logo} alt="user_image" />
+          ) : (
+            <img
+              className={classes.userImg}
+              src={user?.images[0].url}
+              alt="user_image"
+            />
+          )}
 
           <a href="#">{user?.display_name} </a>
         </div>
